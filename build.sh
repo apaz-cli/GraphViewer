3db580caf57559c8492a79b72d2be4ff22d590aa
apaz-cli <aarpazdera@gmail.com>
2024-09-02 19:53:52 -0500

    Update build script

--- build.sh
+++ build.sh
@@ -3,11 +3,12 @@
 # https://github.com/libsdl-org/SDL/releases/latest/download/package.zip
 
 LIBS="-lSDL2 -lSDL2_gfx -lSDL2_ttf -lSDL2_mixer -lm -fopenmp"
-DUMP="-E -P"
 DEBUG="-fsanitize=address -g"
 PROD="-O3 -msse4.2 -march=native -mtune=native"
 
+if [ "$1" = "debug" ]; then PERF="$DEBUG"; else PERF="$PROD"; fi
+
 xxd -i bell.wav  | sed 's/\([0-9a-f]\)$/\0, 0x00/' > bell_wav.xxd
 xxd -i lemon.ttf | sed 's/\([0-9a-f]\)$/\0, 0x00/' > lemon_ttf.xxd
-cc filepicker.c -o filepicker $LIBS $PROD
-cc graph_viewer.c -o graph_viewer $LIBS $PROD
+cc filepicker.c -o filepicker $LIBS $PERF
+cc graph_viewer.c -o graph_viewer $LIBS $PERF
